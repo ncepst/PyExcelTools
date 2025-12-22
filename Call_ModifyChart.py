6a7c9a933dad118d6a7f9f3b48fc01d0f310647a
ncepst <167670837+ncepst@users.noreply.github.com>
2025-12-22 22:39:14 +0900

    Add files via upload

--- Call_ModifyChart.py
+++ Call_ModifyChart.py
@@ -4,39 +4,43 @@ import time
 from ModifyChart import ModifyChart
 
 def main(mode=3):
-
     # 計測開始
     t1 = time.time()
+    try:
+        # 既に開いている Excel に接続
+        wb = xw.books.active
+        ws = wb.sheets.active
+        wb.app.screen_updating = False
 
-    # 既に開いている Excel に接続
-    wb = xw.books.active
-    ws = wb.sheets.active
+        # 既存グラフをインデックスで指定する (0,1,2..)
+        if mode == 1:
+            chart = ws.charts[0]
 
-    # 既存グラフをインデックスで指定する (0,1,2..)
-    if mode == 1:
-        chart = ws.charts[0]
+        # 既存グラフをグラフ名で指定する
+        elif mode == 2:
+            chart = ws.charts("グラフ 1")
 
-    # 既存グラフをグラフ名で指定する
-    elif mode == 2:
-        chart = ws.charts("グラフ 1")
+        # 選択中のグラフを指定する
+        elif mode == 3:
+            app = xw.apps.active
+            chart = app.api.ActiveChart
+            if chart is not None:
+                full_name = chart.Name                    # 例: "Sheet1 グラフ 1"
+                space_index = full_name.find(" ")         # 空白の位置を取得
+                # first_part = full_name[:space_index]
+                second_part = full_name[space_index+1:]   # 空白を除いた残り
+                print ("chart:",second_part)
+                chart = ws.charts(second_part)
+            else:
+                print("アクティブなグラフはありません")
+        
+        ModifyChart(chart,Title="zy",width_cm=12,height_cm=8, Title_space=0,x_major=1)
 
-    # 選択中のグラフを指定する
-    elif mode == 3:
-        app = xw.apps.active
-        chart = app.api.ActiveChart
-        if chart is not None:
-            full_name = chart.Name                      # 例: "Sheet1 グラフ 1"
-            # 空白の位置を取得
-            space_index = full_name.find(" ")
-            # first_part = full_name[:space_index]
-            second_part = full_name[space_index+1:]     # 空白を除いた残り
-            print ("chart:",second_part)
-            chart = ws.charts(second_part)
-        else:
-            print("アクティブなグラフはありません")
+    except :
+        print("Error")
 
-    ModifyChart(chart,Title="測定結果",width_cm=12,height_cm=8, Title_space=+20,x_major=1)
-
+    wb.app.screen_updating = True
+    
     # 計測終了
     t2 = time.time()
     elapsed_time = round(t2-t1,3)
